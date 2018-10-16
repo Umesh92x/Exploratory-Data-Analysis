706ea6d8c3a8a325ee1f66438b658ed9b59af28f
Umesh <37169232+dinesh92x@users.noreply.github.com>
2018-10-16 12:47:09 +0530

    Update linear regression.R

--- linear regression.R
+++ linear regression.R
@@ -1,126 +1,114 @@
+# 1 part is EDA.
+# 2nd part is Model making
+# 3rd is prediction
+# *************************************   EDA   **************************************
+#loading data from R documentation
 data('mtcars')
 cars<-mtcars
+# Just look first 6 row
 head(cars)
+# lets see structre of data
 str(cars)
+# making histogram, box plot and matrix plot
 hist(cars$mpg)
 boxplot(cars$mpg)
 pairs(cars[1:6])
 
-plot(cars$wt,cars$mpg) # scatter plot
-cor(cars$mpg,cars$wt)
-plot(cars$wt,cars$mpg)
-#abline(cars$mpg)
+plot(cars$wt,cars$mpg) # scatter plot,bi variate analysis
+cor(cars$mpg,cars$wt)  # correlation coeff
 #simple linear regression
-carslm1<-lm(mpg~wt, data= cars)
-carslm1$coefficients
-carslm1$fitted.values
+carslm1<-lm(mpg~wt, data= cars)   # Making model linear regression
+carslm1$coefficients              # printing intercept and slop
+carslm1$fitted.values             # print predict value Y cap
 
+plot(cars$wt,carslm1$fitted.values) # plot between x= wt and y= predict value y cap
+abline(carslm1)                     # best fit line
 
-plot(cars$wt,carslm1$fitted.values)
-abline(carslm1)
+RSS1<-sum((cars$mpg-carslm1$fitted.values)^2) # finding RSS, Residual sum of square
+mean(carslm1$residuals)
+#  ***********************Assumption
+# residual must follow mean equal to zeros and 
+# residual must follow normal distribution. So lets test it. using shapiro test
 
-cars$mpg-carslm1$fitted.values
-RSS1<-sum((cars$mpg-carslm1$fitted.values)^2) # finding RSS
-mean(carslm1$residuals)# residual must follow mean equal to zeros 
-# residual must follow normal distribution
-
-shapiro.test(carslm1$residuals)
-
+shapiro.test(carslm1$residuals)    
 mean(cars$mpg)
-TSS<-sum((cars$mpg-20)^2)
-R_sq=(TSS-RSS1)/TSS
+TSS<-sum((cars$mpg-20)^2) # Max error Tss calculated     
+R_sq=(TSS-RSS1)/TSS       # Rsquared values
 R_sq
 
-summary(carslm1)
-summary(carslm1)$r.squared
+summary(carslm1)         # let print individual, group p value, R and R sq value, 
+# RSE and F statistic value and explore these for EDA 
+summary(carslm1)$r.squared    # individal R sq value
 
-summary(carslm1)$adj.r.squared
-RSE<-sqrt(RSS1/(nrow(cars)-ncol(cars)-1))
+summary(carslm1)$adj.r.squared   # individal R adj value
+RSE<-sqrt(RSS1/(nrow(cars)-ncol(cars)-1))   # calculate RSE using FORMULA
 RSE
-summary(carslm1)$sigma
+summary(carslm1)$sigma         # print RES directly 
 
-confint(carslm1)
-plot(carslm1,1)
+confint(carslm1)# finding confidence interval 
+plot(carslm1,1)    # this plot based of resudual value, where is positive and where is negative,this graph look bent because both side in graph of linear model it is postive residula and in middle it is negative residual.
 
 
-#*************************************************
+#************************          MULTIPLE LINEAR REGRESSION
 #multiple linear regression
-carslm2<-lm(mpg~wt+hp,data=cars)
-summary(carslm2)
+carslm2<-lm(mpg~wt+hp,data=cars)  # using weight and hourse power.
+summary(carslm2)                  # print complete set of analysis.
 RSS2<-sum((cars$mpg-carslm2$fitted.values)^2) # finding RSS
 RSS2
 
-plot(carslm2,1)
+plot(carslm2,1) # Plot again residual.
 
-cars$cyl<-as.factor(cars$cyl)
-six<-mean(cars[c(cars$cyl==6),1])
-eight<-mean(cars[c(cars$cyl==8),1])
-four<-mean(cars[c(cars$cyl==4),1])
-six
-eight
-four
-tapply(cars$mpg,cars$cyl,mean)
+cars$cyl<-as.factor(cars$cyl)  # cyl look like categorical in data set but in number so we convert it into categorical. 
+tapply(cars$mpg,cars$cyl,mean) # according to cyl finding mean of mpg
 
-mul_lm<-lm(mpg~cyl,data=cars)
-mul_lm$coefficients
-contrasts(cars$cyl)
-summary(mul_lm)
+mul_lm<-lm(mpg~cyl,data=cars)  # lets take mileage and cylinder.
+mul_lm$coefficients     # print intecet and slop of each variable
+contrasts(cars$cyl)   # find dummy variable.
+summary(mul_lm) # print all analysis
 RSS3<-sum((cars$mpg-mul_lm$fitted.values)^2) # finding RSS
 RSS3
 
-mul_lm2<-lm(mpg~wt+hp+cyl,data=cars)
+mul_lm2<-lm(mpg~wt+hp+cyl,data=cars) # lets take three variable and again do analysis
 mul_lm2
 RSS4<-sum((cars$mpg-mul_lm2$fitted.values)^2) # finding RSS
 RSS4
 summary(mul_lm2)
 
-mul_lm3<-lm(mpg~wt+hp+cyl+wt*hp,data=cars)
+mul_lm3<-lm(mpg~wt+hp+cyl+wt*hp,data=cars) # Again take all three and interaction effect wt*hp 
 mul_lm3$coefficients
 RSS5<-sum((cars$mpg-mul_lm3$fitted.values)^2) # finding RSS
 RSS5
 summary(mul_lm3)
 
-
-mul_lm4<-lm(mpg~wt+hp+wt*hp,data=cars)
+mul_lm4<-lm(mpg~wt+hp+wt*hp,data=cars)  # we removed cyl, because it gives high P value, High RSS and HIGh F statistic 
 mul_lm4$coefficients
 RSS5<-sum((cars$mpg-mul_lm4$fitted.values)^2) # finding RSS
 RSS5
 summary(mul_lm4)
 head(cars)
-#***********************    continue    *****************
-# Polinomial ........
-plot(cars$hp,cars$mpg)
-plot(cars$drat,cars$mpg)
-plot(cars$qsec,cars$mpg)
-plot(cars$disp,cars$mpg)
-
-mul_lm5<-lm(mpg~I(hp^2)+hp,data=cars)
-#OR
-
-RSS6<-sum((cars$mpg-mul_lm4$fitted.values)^2)
-RSS6
-summary(mul_lm4)
-
-mul_lm5<-lm(mpg~I(hp^2)+hp,data=cars)
-RSS7<-sum((cars$mpg-mul_lm5$fitted.values)^2)
+# *************************************   Polinomial 
+plot(cars$hp,cars$mpg)   #lets visualise hp
+mul_lm5<-lm(mpg~I(hp^2)+hp,data=cars) #fitting mpg and hp with power of 2.
+RSS7<-sum((cars$mpg-mul_lm5$fitted.values)^2)  
 RSS7
 summary(mul_lm5)
 
-mul_lm6<-lm(mpg~poly(hp,3)+hp+I(hp^2)+I(hp^3),data=cars)
+mul_lm6<-lm(mpg~poly(hp,3)+hp+I(hp^2)+I(hp^3),data=cars)  # this time power of 3 of hp
 RSS8<-sum((cars$mpg-mul_lm6$fitted.values)^2)
 RSS8
 summary(mul_lm6)
 
-# Adding Hp^2
+# ________________________   After finalise we choose hp with power of 2, it give high f statistic, low RSS, High R adj.
+# Adding Hp^2 to final model
 
-mul_lm7<-lm(mpg~wt+hp+wt*hp+I(hp^2),data=cars)
-mul_lm7$coefficients
+mul_lm7<-lm(mpg~wt+hp+wt*hp+I(hp^2),data=cars) # final model fit.
+mul_lm7$coefficients  # intercetp and slops
 RSS9<-sum((cars$mpg-mul_lm7$fitted.values)^2) # finding RSS
 RSS9
 summary(mul_lm7)
 
 
-mul_lm8<-lm(mpg~.+wt*hp+I(hp^2),data=cars)
+mul_lm8<-lm(mpg~.+wt*hp+I(hp^2),data=cars) # this time chosse every variable, and hp of power of 2.
 mul_lm8$coefficients
 RSS10<-sum((cars$mpg-mul_lm7$fitted.values)^2) # finding RSS
 RSS10
